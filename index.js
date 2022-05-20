6853b48182ebb84a7ede3a164884fb3e94fad1ee
Bui Hoang Long <hoanglongktbv1@gmail.com>
2022-05-20 22:12:51 +0700

    DOM

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-var intro = "Hello world";
-document.getElementById("intro").innerHTML = intro;
+function getValue() {           
+
+    let myInput = document.getElementById("myName");
+    let myName = myInput.value;            
+    
+    alert("Tên của bạn là " + myName); // hiển thị tên của người dùng        
+    
+    }  
